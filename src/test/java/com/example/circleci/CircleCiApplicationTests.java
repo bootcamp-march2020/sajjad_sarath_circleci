395a4f03add92f3025ffc2c15d0c4fb8359aa316
Sajjad Nazrulla <sajjad@Sajjads-MacBook-Pro.local>
2020-03-23 11:12:03 +0530

    Sarath/Sajjad - Add spring project

---
+++ src/test/java/com/example/circleci/CircleCiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.circleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+class CircleCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		fail("Test failed");
+	}
+
+}
